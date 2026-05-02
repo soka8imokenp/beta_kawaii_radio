10852a0a0a20016d678a6b93a93ecf6a8190db42
devvesama <tokimachinedesu@gmail.com>
2026-05-02 16:45:26 +0500

    final cleanup

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Игнорировать ошибки типов при сборке (сайт все равно соберется)
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Игнорировать ошибки линтера тоже, на всякий случай
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
